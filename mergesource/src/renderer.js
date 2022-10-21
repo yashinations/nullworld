e8f1d1d6b2daa7b98b41d9fc900d7da9925dc31c
Aaron Yashinsky <ayashinsky@gmail.com>
2022-10-20 20:55:15 -0700

    camera

--- mergesource/src/renderer.js
+++ mergesource/src/renderer.js
@@ -3,16 +3,19 @@ let last_interval = new Date();
 let camera_scroll_x = 0;
 let camera_scroll_y = 0;
 //hacky!
-let multiplier = 1;//512 / 320;
+let multiplier = 512 / 320;
 
 class renderer_singleton{
 	curr_sprites = [];
 	draw_sprite = function(ctx,s){
 		//expect horizontal sprites
-		ctx.drawImage(s.src_img,s.curr_frame_index * s.frame_width,0,s.frame_width,s.src_img.height,(s.owner.rect.x_coord * multiplier),(s.owner.rect.y_coord * multiplier) - (camera_scroll_y * multiplier),s.owner.rect.width * multiplier,s.owner.rect.height * multiplier);
+		ctx.drawImage(s.src_img, s.curr_frame_index * s.frame_width, 0, s.frame_width, s.src_img.height, (s.owner.rect.x_coord * multiplier) - (camera_scroll_x * multiplier),(s.owner.rect.y_coord * multiplier) - (camera_scroll_y * multiplier),s.owner.rect.width * multiplier,s.owner.rect.height * multiplier);
 		s.next_frame();
 	}
-	render = function(){
+	render = function () {
+		//no magic numbers
+		camera_scroll_x = avatar.rect.x_coord - parseInt(256 / 2);
+		camera_scroll_y = avatar.rect.y_coord - parseInt(256 / 2);
 		if(((new Date()) - last_interval) >= interval_ms) {
 			ctx.clearRect(0,0,canvas.width,canvas.height);		
 			ctx.fillStyle = "lightgray";
@@ -32,7 +35,7 @@ class renderer_singleton{
 				if(b.solid){
 					//ctx.drawImage(art_assets.imgs[2],b.rect.x_coord,b.rect.y_coord, b.rect.width,b.rect.height,(b.rect.x_coord * multiplier),(b.rect.y_coord * multiplier) - (camera_scroll_y * multiplier), b.rect.width * multiplier,b.rect.height * multiplier);
 					ctx.fillStyle = "black";
-					ctx.fillRect((b.rect.x_coord * multiplier),(b.rect.y_coord * multiplier) - (camera_scroll_y * multiplier), b.rect.width * multiplier,b.rect.height * multiplier);
+					ctx.fillRect((b.rect.x_coord * multiplier) - (camera_scroll_x * multiplier),(b.rect.y_coord * multiplier) - (camera_scroll_y * multiplier), b.rect.width * multiplier,b.rect.height * multiplier);
 					ctx.fillStyle = "white";
 				}
 			}
